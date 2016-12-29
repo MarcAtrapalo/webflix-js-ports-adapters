fa6708f814715fe6988da65914fbb697091f2da3
Marc Rodriguez <marc.rodriguez@atrapalo.com>
2016-12-29 16:55:12 +0100

    Boilerplate

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/sinon/index.d.ts" />
+/// <reference path="modules/sinon-chai/index.d.ts" />
