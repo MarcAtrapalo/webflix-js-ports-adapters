938c414fc7414c3a71bcc4a39574cbf8bb4dd0b2
Marc Rodriguez <nebrios2@gmail.com>
2017-02-07 16:13:18 +0100

    Implement InMemoryOrderRepository

---
+++ src/infrastructure/repositories/InMemoryCustomerRepository.ts
@@ -0,0 +1,35 @@
+import ICustomerRepository from '../../application/repository-ports/ICustomerRepository';
+import Customer from '../../domain/model/Customer';
+import InfrastructureException from '../InfrastructureException';
+
+export default class InMemoryCustomerRepository implements ICustomerRepository {
+    private customers: Customer[];
+
+    constructor() {
+        this.customers = [];
+    }
+
+    private getCustomersByName(name: string) {
+        return this.customers.filter((customer: Customer) => (customer.name === name));
+    }
+
+    public add(customer: Customer): void {
+        if (this.getCustomersByName(customer.name).length > 0) {
+            throw new InfrastructureException('Customer already exists');
+        }
+        this.customers.push(customer);
+    }
+
+    public getByName(name: string): Customer {
+        return this.getCustomersByName(name)[0];
+    }
+
+    public update(customer: Customer): void {
+        let customersWithSameName = this.getCustomersByName(customer.name);
+        if (customersWithSameName.length === 0) {
+            throw new InfrastructureException('Customer does not exist');
+        }
+        this.customers = [...this.customers.filter((c) => (c.name !== customer.name)), customer];
+    }
+
+}
